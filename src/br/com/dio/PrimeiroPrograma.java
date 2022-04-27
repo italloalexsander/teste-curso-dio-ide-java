9b0034e8dadbf80f55f23db0e7c13cbc2d5cc522
Itallo <italloalexsander@gmail.com>
2022-04-27 19:18:35 -0300

    First Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		/*int a = 2;
+		int b = 3;
+		System.out.println("hello world " + (a + b));*/
+		
+		Gato gato = new Gato();
+		Livros livro = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livro);
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
